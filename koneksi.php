da4ea8a3a4c8a3223edb6fa61317b6ba48955790
Tsabita Rosyidah Putri <139309070+TsabitaRosyi@users.noreply.github.com>
2026-08-01 22:07:29 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,15 @@
+<!-- // Parameter koneksi ke XAMPP MySQL -->
+
 <?php
 $host = "localhost";
 $user = "root";
 $password = "";
 $database = "tokotsabiphp"; // GANTI dengan nama database kamu
 
+// Membuat koneksi
 $con = mysqli_connect($host, $user, $password, $database);
 
+// Cek apakah koneksi berhasil
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
